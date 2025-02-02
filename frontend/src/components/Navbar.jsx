10c5985006bafc10b3f4a827055ac220f81549f6
anish-garg <anishgarg5914@gmail.com>
2025-02-03 01:16:42 +0530

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,25 @@
+import { Button } from "@/components/ui/button"
+import logo from '../assets/Screenshot_2025-02-03_005651-removebg-preview.png'
+
 const Navbar = () => {
     return (
-        <nav className="flex justify-between">
+        <nav className="flex justify-between items-center bg-white/10 border border-white/20 
+        px-10 py-4 rounded-full backdrop-blur-xl shadow-lg sticky top-0 z-50">
             <div className="flex gap-[4.5rem]">
-                <span>Up Watch</span>
-                <div className="flex gap-12">
+                <div className="flex items-center">
+                    <img src={logo} className="w-11" />
+                    <span>Up Watch</span>
+                </div>
+                <div className="flex gap-12 items-center">
                     <span>Home</span>
                     <span>Monitoring</span>
                     <span>Pricing</span>
                     <span>Documentation</span>
                 </div>
             </div>
-            <div className="flex gap-8">
-                <button>Sign in</button>
-                <button>Sign Up</button>
+            <div className="flex gap-6">
+                <Button className="">Sign in</Button>
+                <Button variant="secondary">Sign up</Button>
             </div>
         </nav>
     )
