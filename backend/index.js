e9f6eee6c44873e6fbc43df9137053a60998cc15
anish-garg <anishgarg5914@gmail.com>
2025-02-13 00:17:13 +0530

    mongodb connected to prisma

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 const app = express();
 
 const PORT = 3000;
@@ -6,6 +9,22 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
+
+async function testMongoDBConnection() {
+    try {
+        await prisma.$connect();
+        console.log("✅ MongoDB connected successfully via Prisma!");
+
+        //   const testRecord = await prisma.yourModelName.findFirst();
+        //   console.log("Sample Record:", testRecord);
+    } catch (error) {
+        console.error("❌ Failed to connect to MongoDB:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+testMongoDBConnection();
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
