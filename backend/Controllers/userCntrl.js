283fb6bb972a2a7a394266fa27e88b604db58e75
anish-garg <anishgarg5914@gmail.com>
2025-02-24 19:58:32 +0530

    monitoring function error resolved

--- backend/Controllers/userCntrl.js
+++ backend/Controllers/userCntrl.js
@@ -2,6 +2,7 @@ import { prisma } from "../config/prismaConfig.js"
 import cron from 'node-cron';
 import axios from 'axios';
 import asyncHandler from 'express-async-handler'
+import { log } from "console";
 
 export const createUser = asyncHandler(async (req, res) => {
     let { email } = req.body;
@@ -22,13 +23,25 @@ export const createUser = asyncHandler(async (req, res) => {
 export const monitorSite = asyncHandler(async (req, res) => {
     try {
         cron.schedule('*/1 * * * *', async () => {
+            console.log("Running the cron job to monitor sites...");
             try {
-                const response = await axios.get('https://github.com');
-                res.send(response.status === 200).send('Site is up');
-            } catch (error) {
-                res.send('Site is down');
+                const users = await prisma.user.findMany({ where: { NOT: [] } });
+                users.forEach(user => {
+                    user.sites.forEach(async (url) => {
+                        try {
+                            const response = await axios.get(url);
+                            res.send(response.status === 200).send('Site is up');
+                        } catch (error) {
+                            res.send('Site is down');
+                        }
+                    })
+                })
+            }
+            catch (error) {
+                console.log('Error fetching sites:', error);
             }
         });
+        res.status(200).send('Cron job started to monitor sites every minute');
     }
     catch (error) {
         console.log(error);
