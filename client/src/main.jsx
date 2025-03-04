e5132d1ca8ec372cb867199c8b36b68c584a1d78
anish-garg <anishgarg5914@gmail.com>
2025-03-05 04:24:08 +0530

    added google auth for sign up

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-bjwz0an11tmivxrs.us.auth0.com"
+      clientId="16LPTDFQRu0rJ6BnDJ4SepDfG1luzAcs"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
