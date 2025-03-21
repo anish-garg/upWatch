9e04cffaf77504100a645812273d9ff7e6e11e91
anish-garg <anishgarg5914@gmail.com>
2025-03-21 19:27:52 +0530

    vercel url corrected

--- client/src/utilities/api.js
+++ client/src/utilities/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8000/api/user" || "https://up-watch.vercel.app/api/user"
+    baseURL: "http://localhost:8000/api/user" || "https://up-watch.vercel.app"
 })
 
 export const createUser = async (formData) => {
